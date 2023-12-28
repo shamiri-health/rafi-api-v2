0747c7af258d234453cd5dbc98862ee7e3531699
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-28 16:13:21 +0300

    twillio client

--- src/plugins/twillio.ts
+++ src/plugins/twillio.ts
@@ -1,8 +1,19 @@
-// TODO: should be stubbed by sinon when testin
+// TODO: should be stubbed by sinon when testing
 import twillio from "twilio";
+import fp from "fastify-plugin";
 
-const client = twillio(
+export const twilioClient = twillio(
   process.env.TWILLIO_ACCOUNT_SID,
   process.env.TWILLIO_AUTH_TOKEN,
   { autoRetry: true, maxRetries: 10 },
 );
+
+export default fp(async (fastify, _) => {
+  fastify.decorate("twilio", twilioClient);
+});
+
+declare module "fastify" {
+  export interface FastifyInterface {
+    twilio: typeof twilioClient;
+  }
+}
