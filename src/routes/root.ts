96c4ad0a2ad36f8b5c4f29b83e8f20931c455c2d
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-28 12:16:33 +0300

    test route

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,7 +1,11 @@
 import { FastifyPluginAsync } from "fastify";
+import { user } from "../schema";
 
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request, reply) {
+const root: FastifyPluginAsync = async (fastify, _): Promise<void> => {
+  fastify.get("/", async function (request, _) {
+    fastify.log.info(fastify.db);
+    const t = await fastify.db.select().from(user);
+    fastify.log.info(t);
     return { root: true };
   });
 };
