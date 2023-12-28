4c8a2b51a69adc935bd5c1cb1cd243acf27e5ddd
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-28 14:52:55 +0300

    ensure that tests are passing

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsync } from "fastify";
 import { blacklistToken } from "../../schema";
+import { Type } from "@sinclair/typebox";
 
 const authRouther: FastifyPluginAsync = async (
   fastify,
@@ -17,35 +18,44 @@ const authRouther: FastifyPluginAsync = async (
 
   fastify.post("/forgot-pin", async (request, response) => {});
 
-  fastify.post("/logout", async (request, _) => {
-    const authorizationHeader =
-      request.headers.authorization ||
-      (request.headers.Authorization as string);
-    if (authorizationHeader) {
-      try {
-        const [bearer, token] = authorizationHeader.split(" ");
+  fastify.post(
+    "/logout",
+    {
+      schema: {
+        response: {
+          200: Type.Object({ message: Type.String() }),
+        },
+      },
+    },
+    async (request, _) => {
+      const authorizationHeader = (request.headers.authorization ||
+        request.headers.Authorization) as string;
+      if (authorizationHeader) {
+        try {
+          const [bearer, token] = authorizationHeader.split(" ");
 
-        if (bearer.trim() !== "Bearer") {
-          throw fastify.httpErrors.badRequest("Incorrect header format");
+          if (bearer.trim() !== "Bearer") {
+            throw fastify.httpErrors.badRequest("Incorrect header format");
+          }
+
+          await fastify.db.insert(blacklistToken).values({
+            token: token.trim(),
+            blacklistedOn: new Date().toISOString(),
+          });
+
+          return { message: "successfully logged out user" };
+        } catch (e) {
+          fastify.log.error(e);
+          throw fastify.httpErrors.badRequest(
+            "Unable to parse token, please provide a valid header in the form of Bearer <Token>",
+          );
         }
-
-        await fastify.db.insert(blacklistToken).values({
-          token: token.trim(),
-          blacklistedOn: new Date().toISOString(),
-        });
-
-        return { message: "sucessfully logged out user" };
-      } catch (e) {
-        fastify.log.error(e);
-        throw fastify.httpErrors.badRequest(
-          "Unable to parse token, please provided a valid header in the form of Bearer <Token>",
-        );
       }
-    }
-    throw fastify.httpErrors.badRequest(
-      "Cannot logout a user without a valid Authorization",
-    );
-  });
+      throw fastify.httpErrors.badRequest(
+        "Cannot logout a user without a valid Authorization",
+      );
+    },
+  );
 };
 
 export default authRouther;
--- test/routes/auth.test.ts
+++ test/routes/auth.test.ts
@@ -4,20 +4,23 @@ import { eq } from "drizzle-orm";
 import { blacklistToken } from "../../src/schema";
 
 test("should be able to logout a user given a token in req.headers.authroziation", async (t) => {
+  t.plan(3);
   const app = await build(t);
+
+  const tokenText = "somethingRandom";
 
   const res = await app
     .inject()
     .post("/auth/logout")
-    .headers({ Authorization: "Bearer someRandomStuff " });
+    .headers({ Authorization: `Bearer ${tokenText}` });
 
-  t.equal(res.status, 200);
-  t.equal(res.body, { message: "sucessfully logged out user" });
+  t.equal(res.statusCode, 200);
+  t.match(JSON.parse(res.payload), { message: "successfully logged out user" });
 
   const dbResult = await app.db
     .select()
     .from(blacklistToken)
-    .where(eq(blacklistToken.token, "someRandomStuff"));
+    .where(eq(blacklistToken.token, tokenText));
 
-  t.strictSame(dbResult[0].token, "somRandomStuff");
+  t.equal(dbResult[0].token, tokenText);
 });
