a107b29833bee961aa097678f722fe0555036ff6
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-30 12:10:13 +0300

    add logic for /verify endpoint

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,9 +1,20 @@
 import { FastifyPluginAsync } from "fastify";
-import { blacklistToken } from "../../schema";
+import { blacklistToken, human } from "../../schema";
 import { Type, Static } from "@sinclair/typebox";
 import { isValidPhoneNumber } from "libphonenumber-js";
 import { sendVerificationCode } from "../../lib/auth";
 import { z } from "zod";
+import { eq, or } from "drizzle-orm";
+
+// TODO: harden validation here
+export const VerifyTokenBody = Type.Object({
+  phone_number: Type.Optional(Type.String()),
+  phoneNumber: Type.Optional(Type.String()),
+  email: Type.Optional(Type.String()),
+  channel: Type.Union([Type.Literal("sms"), Type.Literal("email")]),
+});
+
+export type VerifyTokenBody = Static<typeof VerifyTokenBody>;
 
 export const ForgotPinResponse = Type.Object({
   username: Type.String(),
@@ -12,15 +23,58 @@ export const ForgotPinResponse = Type.Object({
 export type ForgotPinResponseType = Static<typeof ForgotPinResponse>;
 
 const authRouther: FastifyPluginAsync = async (fastify, _): Promise<void> => {
-  fastify.post("/verify", async () => {});
+  fastify.post<{ Body: VerifyTokenBody }>("/verify", async (request) => {
+    const { phoneNumber, phone_number, email, channel } = request.body;
+
+    if (!phoneNumber || (!phone_number && channel === "sms")) {
+      throw fastify.httpErrors.badRequest(
+        "You need to specify either phone_number or phoneNumber if the specified channel is sms",
+      );
+    }
+
+    if (!email && channel === "email") {
+      throw fastify.httpErrors.badRequest("You need to specify eithe");
+    }
+
+    // TODO: deprecate this once we use the correct key i.e. phone_number
+    const phoneValue = phoneNumber ?? phone_number;
+    const result = await fastify.db
+      .select()
+      .from(human)
+      .where(or(eq(human.email, email), eq(human.mobile, phoneValue)));
+
+    if (!result.length) {
+      throw fastify.httpErrors.notFound(
+        "Could not find a user with the specified email",
+      );
+    }
+
+    try {
+      if (channel === "sms") {
+        await sendVerificationCode(phoneValue, "sms");
+      } else {
+        await sendVerificationCode(phoneValue, "email");
+      }
+      return { message: "Verification token sent successfully" };
+    } catch (e) {
+      fastify.log.error(e);
+      throw fastify.httpErrors.internalServerError(
+        "Sorry could not process request. Please contact software support",
+      );
+    }
+  });
 
   fastify.post("/token", async () => {});
 
   fastify.post("/create-user", async () => {});
 
-  fastify.post("/forgotPin", async (_, response) => {
-    response.redirect(302, "/forgot-pin");
-  });
+  fastify.post(
+    "/forgotPin",
+    { schema: { deprecated: true } },
+    async (_, response) => {
+      response.redirect(302, "/forgot-pin");
+    },
+  );
 
   fastify.post<{ Body: ForgotPinResponseType }>(
     "/forgot-pin",
