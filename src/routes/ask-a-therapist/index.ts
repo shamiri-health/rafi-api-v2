0fb9387535ea01ddd68c794c5bb185d9edb3520d
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-04 12:32:42 +0300

    add more routes, fix formatting issues and add ask a therapist question bank

--- src/routes/ask-a-therapist/index.ts
+++ src/routes/ask-a-therapist/index.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { FastifyPluginAsync } from "fastify";
 import { answers, questions } from "../../database/schema";
 import { Static, Type } from "@sinclair/typebox";
@@ -17,8 +17,16 @@ const AnswerBody = Type.Object({
   question_id: Type.Optional(Type.String()),
 });
 
+const QuestionFetchParams = Type.Object({
+  question_id: Type.String(),
+});
+
+const QuestionUpdateBody = Type.Pick(QuestionBody, ["question"]);
+
 type QuestionBody = Static<typeof QuestionBody>;
 type AnswerBody = Static<typeof AnswerBody>;
+type QuestionFetchParams = Static<typeof QuestionFetchParams>;
+type QuestionUpdateBody = Static<typeof QuestionUpdateBody>;
 
 const askATherapistRouter: FastifyPluginAsync = async (
   fastify,
@@ -26,15 +34,15 @@ const askATherapistRouter: FastifyPluginAsync = async (
   // TODO: implement pagination
   // @ts-ignore
   fastify.get("/", { onRequest: fastify.authenticate }, async (request) => {
-    //@ts-ignore
     return await fastify.db.query.questions.findMany({
+      // @ts-ignore
       where: eq(questions.id, request.user.sub),
     });
   });
 
-  // @ts-ignore
   fastify.post<{ Body: QuestionBody }>(
     "/question",
+    // @ts-ignore
     { onRequest: fastify.authenticate, schema: { body: QuestionBody } },
     async (request) => {
       const id = randomUUID() as string;
@@ -55,9 +63,9 @@ const askATherapistRouter: FastifyPluginAsync = async (
   );
 
   // TODO: also add the therapist ID to track who answered the question
-  // @ts-ignore
   fastify.post<{ Body: AnswerBody }>(
     "/answer",
+    // @ts-ignore
     { onRequest: fastify.authenticate, schema: { body: AnswerBody } },
     async (request) => {
       const id = randomUUID() as string;
@@ -70,6 +78,91 @@ const askATherapistRouter: FastifyPluginAsync = async (
       return postedAnswer;
     },
   );
+
+  // FIXME: should also return the answer
+  fastify.get<{ Params: QuestionFetchParams }>(
+    "/question/:question_id",
+    // @ts-ignore
+    {
+      onRequest: fastify.authenticate,
+      schema: { params: QuestionFetchParams },
+    },
+    async (request) => {
+      const existingQuestion = await fastify.db.query.questions.findFirst({
+        where: and(
+          eq(questions.id, request.params.question_id),
+          // @ts-ignore
+          eq(questions.userId, request.user.sub),
+        ),
+      });
+
+      if (!existingQuestion) {
+        throw fastify.httpErrors.notFound(
+          `Question with id: ${request.params.question_id} not found for this user`,
+        );
+      }
+
+      return existingQuestion;
+    },
+  );
+
+  fastify.put<{ Params: QuestionFetchParams; Body: QuestionUpdateBody }>(
+    "/question/:question_id",
+    {
+      onRequest: fastify.authenticate,
+      schema: { body: QuestionUpdateBody, params: QuestionFetchParams },
+    },
+    async (request) => {
+      const [updatedQuestion] = await fastify.db
+        .update(questions)
+        .set({
+          question: request.body.question,
+        })
+        .where(
+          and(
+            eq(questions.id, request.params.question_id),
+            eq(questions.userId, request.user.sub),
+          ),
+        )
+        .returning();
+
+      if (!updatedQuestion) {
+        throw fastify.httpErrors.notFound(
+          `Question with id: ${request.params.question_id} not found for current user`,
+        );
+      }
+
+      return updatedQuestion;
+    },
+  );
+
+  fastify.delete<{ Params: QuestionFetchParams }>(
+    "/question/:question_id",
+    {
+      onRequest: fastify.authenticate,
+      schema: { params: QuestionFetchParams },
+    },
+    async (request) => {
+      try {
+        // @ts-ignore
+        await fastify.db
+          .delete(questions)
+          .where(
+            and(
+              eq(questions.id, request.params.question_id),
+              eq(questions.userId, request.user.sub),
+            ),
+          );
+        fastify.log.info("Successfully deleted question");
+      } catch (e) {
+        fastify.log.error("Could not deleter");
+      }
+
+      return {};
+    },
+  );
+
+  fastify.get("/community-questions", async (request) => {});
 };
 
 export default askATherapistRouter;
