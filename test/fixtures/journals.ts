3f105edda475c3fa19974dc4e99364cd9e385ebf
UwaoEvan <uwaoevanz@gmail.com>
2024-03-14 14:08:51 +0300

    code formatting

--- test/fixtures/journals.ts
+++ test/fixtures/journals.ts
@@ -4,21 +4,22 @@ import { journal } from "../../src/database/schema";
 import { database } from "../../src/lib/db";
 
 export const generateJournalEntry = async (
-    db: database["db"],
-    userId: number
+  db: database["db"],
+  userId: number,
 ) => {
-    const [createdJournalEntry] = await db
+  const [createdJournalEntry] = await db
     .insert(journal)
     .values({
-        id: randomUUID(),
-        content1: faker.lorem.sentence(),
-        question1: faker.lorem.sentence(),
-        question2: faker.lorem.sentence(),
-        content2: faker.lorem.sentence(),
-        content3: faker.lorem.sentence(),
-        question3: faker.lorem.sentence(),
-        userId
-    }).returning();
+      id: randomUUID(),
+      content1: faker.lorem.sentence(),
+      question1: faker.lorem.sentence(),
+      question2: faker.lorem.sentence(),
+      content2: faker.lorem.sentence(),
+      content3: faker.lorem.sentence(),
+      question3: faker.lorem.sentence(),
+      userId,
+    })
+    .returning();
 
-    return createdJournalEntry;
-}
+  return createdJournalEntry;
+};
--- test/routes/journal/update-journal-entry.test.ts
+++ test/routes/journal/update-journal-entry.test.ts
@@ -7,63 +7,60 @@ import { generateUser } from "../../fixtures/users";
 import { generateJournalEntry } from "../../fixtures/journals";
 
 test("PUT /journaling/:journal_id should update the current journal entry", async (t) => {
-    // given
-    const app = await build(t);
-    const sampleUser = await generateUser(app.db);
-    const token = await encodeAuthToken(sampleUser.id, "user");
-    const sampleJournalEntry = await generateJournalEntry(app.db, sampleUser.id);
+  // given
+  const app = await build(t);
+  const sampleUser = await generateUser(app.db);
+  const token = await encodeAuthToken(sampleUser.id, "user");
+  const sampleJournalEntry = await generateJournalEntry(app.db, sampleUser.id);
 
-    t.teardown(async () => {
-        await app.db.delete(journal).where(eq(journal.userId, sampleUser.id));
-        await app.db.delete(user).where(eq(user.id, sampleUser.id));
-    })
+  t.teardown(async () => {
+    await app.db.delete(journal).where(eq(journal.userId, sampleUser.id));
+    await app.db.delete(user).where(eq(user.id, sampleUser.id));
+  });
 
-    const payload = {
-        question_1: "Oyaaaaaaa",
-        content_1: "Halooo"
-    };
+  const payload = {
+    question_1: "Oyaaaaaaa",
+    content_1: "Halooo",
+  };
 
-    // when
-    const response = await app
+  // when
+  const response = await app
     .inject()
-    .headers({ authorization: `Bearer ${token}`})
+    .headers({ authorization: `Bearer ${token}` })
     .put(`/journaling/${sampleJournalEntry.id}`)
-    .payload(payload)    
+    .payload(payload);
 
-    const body = await response.json();
+  const body = await response.json();
 
-    // then
-    const updatedJournalEntry = await app.db.query.journal.findFirst({
-        where: and(
-            eq(journal.userId, sampleUser.id),
-            eq(journal.id, body.id)
-        )
-    })
-    
-    t.equal(response.statusCode, 200);
-    t.ok(updatedJournalEntry);
-    t.equal(updatedJournalEntry.question1, payload.question_1);
-    t.equal(updatedJournalEntry.content1, payload.content_1);
-})
+  // then
+  const updatedJournalEntry = await app.db.query.journal.findFirst({
+    where: and(eq(journal.userId, sampleUser.id), eq(journal.id, body.id)),
+  });
 
- test("PUT /journaling/:journal_id should 401 if the user is not authenticated", async (t) => {
-    // given
-    const app = await build(t);
-    const sampleUser = await generateUser(app.db);
-    const sampleJournalEntry = await generateJournalEntry(app.db, sampleUser.id);
+  t.equal(response.statusCode, 200);
+  t.ok(updatedJournalEntry);
+  t.equal(updatedJournalEntry.question1, payload.question_1);
+  t.equal(updatedJournalEntry.content1, payload.content_1);
+});
 
-    const payload = {
-        question_1: "Oyaaaaaaa",
-        content_1: "Halooo"
-    };
+test("PUT /journaling/:journal_id should 401 if the user is not authenticated", async (t) => {
+  // given
+  const app = await build(t);
+  const sampleUser = await generateUser(app.db);
+  const sampleJournalEntry = await generateJournalEntry(app.db, sampleUser.id);
 
-    // when
-    const response = await app
+  const payload = {
+    question_1: "Oyaaaaaaa",
+    content_1: "Halooo",
+  };
+
+  // when
+  const response = await app
     .inject()
     .put(`/journaling/${sampleJournalEntry.id}`)
     .payload(payload);
 
-    // then
-    t.equal(response.statusMessage, "Unauthorized");
-    t.equal(response.statusCode, 401);
- })
+  // then
+  t.equal(response.statusMessage, "Unauthorized");
+  t.equal(response.statusCode, 401);
+});
