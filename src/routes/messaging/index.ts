e776663ecc14e8ce1332fa33840547a059240f3e
UwaoEvan <uwaoevanz@gmail.com>
2024-03-21 12:58:38 +0300

    formatting

--- src/routes/messaging/index.ts
+++ src/routes/messaging/index.ts
@@ -3,49 +3,54 @@ import { FastifyPluginAsync } from "fastify";
 import { Static, Type } from "@sinclair/typebox";
 
 const MessageBody = Type.Object({
-    clientId: Type.String(),
-    title: Type.String(),
-    body: Type.String()
+  clientId: Type.String(),
+  title: Type.String(),
+  body: Type.String(),
 });
 
-type MessageBody = Static <typeof MessageBody>;
+type MessageBody = Static<typeof MessageBody>;
 
 const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "");
-const serviceAccount = serviceKey as admin.ServiceAccount
+const serviceAccount = serviceKey as admin.ServiceAccount;
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const messagingRouter: FastifyPluginAsync = async (fastify, _): Promise<void> => {
-    fastify.post<{ Body: MessageBody }>("/", 
-        {
-            schema: {
-                body: MessageBody
-            }
-        }, 
-        async (request) => {
-            const { clientId, title, body } = request.body;
-            
-            // the message structure
-            const message = {
-                data: {
-                    title,
-                    body,
-                    clientId,
-                },
-                condition: "'rafiMessages' in topics", // do not change since this topic is hardcoded on the client app.
-            }
-            
-            // try sending the message
-            admin.messaging()
-            .send(message)
-            .catch(error => {
-                fastify.log.error(error);
-                throw error;
-            })
-        }
-    )
-}
+const messagingRouter: FastifyPluginAsync = async (
+  fastify,
+  _,
+): Promise<void> => {
+  fastify.post<{ Body: MessageBody }>(
+    "/",
+    {
+      schema: {
+        body: MessageBody,
+      },
+    },
+    async (request) => {
+      const { clientId, title, body } = request.body;
+
+      // the message structure
+      const message = {
+        data: {
+          title,
+          body,
+          clientId,
+        },
+        condition: "'rafiMessages' in topics", // do not change since this topic is hardcoded on the client app.
+      };
+
+      // try sending the message
+      admin
+        .messaging()
+        .send(message)
+        .catch((error) => {
+          fastify.log.error(error);
+          throw error;
+        });
+    },
+  );
+};
 
 export default messagingRouter;
