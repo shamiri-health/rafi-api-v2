f7abb59a0f6f2724d3008221c2a1b94be5f1a61b
UwaoEvan <uwaoevanz@gmail.com>
2024-04-19 12:49:52 +0300

    Migrated the therapist assignment endpoint

--- src/routes/therapists/index.ts
+++ src/routes/therapists/index.ts
@@ -1,7 +1,14 @@
 import { FastifyPluginAsync } from "fastify";
 import { Static, Type } from "@sinclair/typebox";
-import { human, therapist } from "../../database/schema";
 import { eq } from "drizzle-orm";
+
+import { human, therapist, therapySession, user } from "../../database/schema";
+import { 
+    recommendGroupSession, 
+    recommendOnsiteSession, 
+    recommendShamiriDigitalSession, 
+    recommendTeleTherapySession 
+} from "../../lib/therapySession";
 
 const TherapistBase = Type.Object({
     name: Type.String(),
@@ -31,6 +38,16 @@ const TherapistCreate = Type.Object({
     photoUrl: Type.String()
 })
 
+const TherapistAssignment = Type.Object({
+    userId: Type.Integer(),
+    eventId: Type.String(),
+    shamiriDigitalRecommendation: Type.Optional(Type.String()),
+    therapistRecommendation: Type.Optional(Type.String()),
+    externalRecommendation: Type.Optional(Type.String()),
+    psychologistRecommendation: Type.Optional(Type.String()),
+    groupTherapyRecommendation: Type.Optional(Type.String())
+})
+
 const TherapistParams = Type.Object({
     therapistId: Type.Integer()
 })
@@ -39,6 +56,7 @@ type Therapist = Static<typeof Therapist>;
 type TherapistBase = Static<typeof TherapistBase>;
 type TherapistCreate = Static<typeof TherapistCreate>;
 type TherapistParams = Static<typeof TherapistParams>;
+type TherapistAssignment = Static<typeof TherapistAssignment>;
 
 const therapists: FastifyPluginAsync = async (fastify, _): Promise<void> => {
     // @ts-ignore
@@ -157,6 +175,77 @@ const therapists: FastifyPluginAsync = async (fastify, _): Promise<void> => {
         }
         return therapistInformation;
     })
+
+    fastify.post<{ Body: TherapistAssignment }>("/assignment", 
+        {
+            schema: {
+                body: TherapistAssignment
+            }
+        }, 
+        async (request, reply) => {
+            const {
+                userId,
+                eventId,
+                shamiriDigitalRecommendation,
+                therapistRecommendation,
+                groupTherapyRecommendation
+            } = request.body;
+
+            const enrolledTherapySession = await fastify.db.query.therapySession.findFirst({
+                where: eq(therapySession.id, eventId)
+            })
+
+            const targetUser = await fastify.db.query.user.findFirst({
+                where: eq(user.id, userId)
+            });
+
+            if (!targetUser) {
+                throw fastify.httpErrors.notFound(
+                    `User with the id of ${userId} not found.`
+                )
+            }
+
+            if (shamiriDigitalRecommendation) {
+                const recommendedSession = await recommendShamiriDigitalSession(
+                    fastify.db, 
+                    userId, 
+                    parseInt(shamiriDigitalRecommendation.trim())
+                );
+                
+                return reply.code(201).send(recommendedSession);
+            }
+
+            if (therapistRecommendation) {
+                if (enrolledTherapySession?.type === "phoneEvent") {
+                    const teleTherapySession = await recommendTeleTherapySession(
+                        fastify.db,
+                        userId,
+                        parseInt(therapistRecommendation)
+                    );
+
+                    return reply.code(201).send(teleTherapySession);
+                } else {
+                    const onsiteSession = await recommendOnsiteSession(
+                        fastify.db,
+                        userId,
+                        parseInt(therapistRecommendation)
+                    )
+
+                    return reply.code(201).send(onsiteSession);
+                }
+            }
+
+            if (groupTherapyRecommendation) {
+                const groupSession = await recommendGroupSession(
+                    fastify.db,
+                    userId,
+                    parseInt(groupTherapyRecommendation)
+                )
+
+                return groupSession;
+            }
+        }
+    )
 }
 
 export default therapists;
