6232ce45e24ce067bc3fc0436364cd81b50d5e29
UwaoEvan <uwaoevanz@gmail.com>
2024-04-30 12:30:29 +0300

    daily checkin endpoints

--- src/lib/userRewardHub.ts
+++ src/lib/userRewardHub.ts
@@ -1,60 +1,69 @@
-import { Static, Type } from "@sinclair/typebox"
+import { Static, Type } from "@sinclair/typebox";
 import { eq } from "drizzle-orm";
 import { database } from "./db";
 import { userRewardHub } from "../database/schema";
 
 const RewardHub = Type.Object({
-    id: Type.Number(),
-    gemHave: Type.Number(),
-    level: Type.Number()
-})
+  id: Type.Number(),
+  gemHave: Type.Number(),
+  level: Type.Number(),
+});
 
 type RewardHub = Static<typeof RewardHub>;
 
 interface GemLevel {
-    [level: number]: [level: string, gems: number]
+  [level: number]: [level: string, gems: number];
 }
 const gemsLevel: GemLevel = {
-    0: ["", 0],
-    1: ["Worm", 5],
-    2: ["Samaki", 30],
-    3: ["Chick", 80],
-    4: ["Twiga", 200],
-    5: ["Tumbili", 350],
-    6: ["Pundamilia", 550],
-    7: ["Pomboo", 800],
-    8: ["Chui", 1100],
-    9: ["Tembo", 1550],
-    10: ["Unicorn", 2000]
-}
+  0: ["", 0],
+  1: ["Worm", 5],
+  2: ["Samaki", 30],
+  3: ["Chick", 80],
+  4: ["Twiga", 200],
+  5: ["Tumbili", 350],
+  6: ["Pundamilia", 550],
+  7: ["Pomboo", 800],
+  8: ["Chui", 1100],
+  9: ["Tembo", 1550],
+  10: ["Unicorn", 2000],
+};
 
-export const addGems = async (db: database['db'], record: RewardHub, nGems: number) => {
-    const totalGems: number = record.gemHave + nGems;
-    const gemsNextLevel: number = gemsLevel[record.level + 1][1];
+export const addGems = async (
+  db: database["db"],
+  record: RewardHub,
+  nGems: number,
+) => {
+  const totalGems: number = record.gemHave + nGems;
+  const gemsNextLevel: number = gemsLevel[record.level + 1][1];
 
-    await db.update(userRewardHub)
+  await db
+    .update(userRewardHub)
     .set({ gemsHave: totalGems })
-    .where(eq(userRewardHub.id, record.id))
-    
-    if (totalGems >= gemsNextLevel) {
-        // unlock next level
-        return await unlockNextLevel(db, record);
-    }
-    return [];
-}
+    .where(eq(userRewardHub.id, record.id));
 
-export const unlockNextLevel = async (db: database["db"], record: RewardHub) => {
-    if (record.level > 9) return;
-    const nextLevel: number = record.level + 1;
+  if (totalGems >= gemsNextLevel) {
+    // unlock next level
+    return await unlockNextLevel(db, record);
+  }
+  return [];
+};
 
-    const [updatedRecord] = await db.update(userRewardHub)
+export const unlockNextLevel = async (
+  db: database["db"],
+  record: RewardHub,
+) => {
+  if (record.level > 9) return;
+  const nextLevel: number = record.level + 1;
+
+  const [updatedRecord] = await db
+    .update(userRewardHub)
     .set({ level: nextLevel })
     .where(eq(userRewardHub.id, record.id))
     .returning();
 
-    return {
-        displayText: `You have just reached to level ${updatedRecord.level}`,
-        level: updatedRecord.level,
-        achievement: `achLevel ${updatedRecord.level}`
-    }
-}
+  return {
+    displayText: `You have just reached to level ${updatedRecord.level}`,
+    level: updatedRecord.level,
+    achievement: `achLevel ${updatedRecord.level}`,
+  };
+};
