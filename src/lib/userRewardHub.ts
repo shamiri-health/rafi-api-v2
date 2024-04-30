2897f5d4e34af212be01ef2ce265d0be39b2572c
UwaoEvan <uwaoevanz@gmail.com>
2024-04-30 11:35:18 +0300

    Created utils for updating gems and levels respectively

--- src/lib/userRewardHub.ts
+++ src/lib/userRewardHub.ts
@@ -1,63 +1,60 @@
-// def gemsNextLevel(self):
-//         gems_level = {
-//             0: ("", 0),
-//             1: ("Worm", 5),
-//             2: ("Samaki", 30),
-//             3: ("Chick", 80),
-//             4: ("Twiga", 200),
-//             5: ("Tumbili", 350),
-//             6: ("Pundamilia", 550),
-//             7: ("Pomboo", 800),
-//             8: ("Chui", 1100),
-//             9: ("Tembo", 1550),
-//             10: ("Unicorn", 2000),
-//         }
-//         return gems_level[self.level + 1][1]
-
 import { Static, Type } from "@sinclair/typebox"
+import { eq } from "drizzle-orm";
 import { database } from "./db";
 import { userRewardHub } from "../database/schema";
-import { eq } from "drizzle-orm";
 
-//     def __repr__(self):
-//         return "<UserRewardHub %r>" % self.id
-
-//     def level_up(self):
-//         if self.level > 9:
-//             return None
-//         self.level = self.level + 1
-//         achievement = self.userAchievement
-//         setattr(achievement, "achLevel" + str(self.level), datetime.utcnow())
-//         resp = {
-//             "displayText": "You have just reached to level " + str(self.level),
-//             "level": self.level,
-//             "achievement": "achLevel" + str(self.level),
-//         }
-//         return resp
-
-// def add_gems(self, nGems):
-//         response = {}
-//         self.gemsHave = self.gemsHave + nGems
-//         if self.gemsHave >= self.gemsNextLevel:
-//             response = self.level_up()
-//         if response:
-//             return [response]
-//         else:
-//             return []
 const RewardHub = Type.Object({
     id: Type.Number(),
-    gemHave: Type.Number()
+    gemHave: Type.Number(),
+    level: Type.Number()
 })
+
 type RewardHub = Static<typeof RewardHub>;
+
+interface GemLevel {
+    [level: number]: [level: string, gems: number]
+}
+const gemsLevel: GemLevel = {
+    0: ["", 0],
+    1: ["Worm", 5],
+    2: ["Samaki", 30],
+    3: ["Chick", 80],
+    4: ["Twiga", 200],
+    5: ["Tumbili", 350],
+    6: ["Pundamilia", 550],
+    7: ["Pomboo", 800],
+    8: ["Chui", 1100],
+    9: ["Tembo", 1550],
+    10: ["Unicorn", 2000]
+}
+
 export const addGems = async (db: database['db'], record: RewardHub, nGems: number) => {
-    const totalGems = record.gemHave + nGems;
-    const gemsNextLevel = 0;
+    const totalGems: number = record.gemHave + nGems;
+    const gemsNextLevel: number = gemsLevel[record.level + 1][1];
 
     await db.update(userRewardHub)
     .set({ gemsHave: totalGems })
     .where(eq(userRewardHub.id, record.id))
     
     if (totalGems >= gemsNextLevel) {
+        // unlock next level
+        return await unlockNextLevel(db, record);
+    }
+    return [];
+}
 
+export const unlockNextLevel = async (db: database["db"], record: RewardHub) => {
+    if (record.level > 9) return;
+    const nextLevel: number = record.level + 1;
+
+    const [updatedRecord] = await db.update(userRewardHub)
+    .set({ level: nextLevel })
+    .where(eq(userRewardHub.id, record.id))
+    .returning();
+
+    return {
+        displayText: `You have just reached to level ${updatedRecord.level}`,
+        level: updatedRecord.level,
+        achievement: `achLevel ${updatedRecord.level}`
     }
 }
