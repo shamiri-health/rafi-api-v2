87c15480cd72468bdf7a40dde626745443a2023b
UwaoEvan <uwaoevanz@gmail.com>
2024-04-30 12:30:29 +0300

    daily checkin endpoints

--- src/routes/daily-check-in/index.ts
+++ src/routes/daily-check-in/index.ts
@@ -1,139 +1,148 @@
 import { FastifyPluginAsync } from "fastify";
 import CHECKINPROMTS from "../../../static/daily_checkin_prompts.json";
 import { and, eq, sql } from "drizzle-orm";
-import { dailyCheckIn, userAchievement, userRewardHub } from "../../database/schema";
+import {
+  dailyCheckIn,
+  userAchievement,
+  userRewardHub,
+} from "../../database/schema";
 import { Static, Type } from "@sinclair/typebox";
 import { randomUUID } from "crypto";
 
 const DailyCheckInSubmission = Type.Object({
-    how_are_you_feeling: Type.String(),
-    mood_description: Type.String(),
-    mood_description_cause_category_1: Type.String(),
-    mood_description_cause_response_1: Type.String(),
-    mood_description_cause_category_2: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_response_2: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_category_3: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_response_3: Type.Optional(Type.Array(Type.String())),
-})
+  how_are_you_feeling: Type.String(),
+  mood_description: Type.String(),
+  mood_description_cause_category_1: Type.String(),
+  mood_description_cause_response_1: Type.String(),
+  mood_description_cause_category_2: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_response_2: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_category_3: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_response_3: Type.Optional(Type.Array(Type.String())),
+});
 
 const DailyCheckInResponse = Type.Object({
-    id: Type.String(),
-    created_at: Type.String({ format: "date-time" }),
-    updated_at: Type.String({ format: "date-time" }),
-    how_are_you_feeling: Type.String(),
-    mood_description: Type.String(),
-    mood_description_cause_category_1: Type.String(),
-    mood_description_cause_response_1: Type.String(),
-    mood_description_cause_category_2: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_response_2: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_category_3: Type.Optional(Type.Array(Type.String())),
-    mood_description_cause_response_3: Type.Optional(Type.Array(Type.String())),
-    gems: Type.Integer(),
-    streak: Type.Integer(),
-    prompt_weekly: Type.Boolean()
-})
+  id: Type.String(),
+  created_at: Type.String({ format: "date-time" }),
+  updated_at: Type.String({ format: "date-time" }),
+  how_are_you_feeling: Type.String(),
+  mood_description: Type.String(),
+  mood_description_cause_category_1: Type.String(),
+  mood_description_cause_response_1: Type.String(),
+  mood_description_cause_category_2: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_response_2: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_category_3: Type.Optional(Type.Array(Type.String())),
+  mood_description_cause_response_3: Type.Optional(Type.Array(Type.String())),
+  gems: Type.Integer(),
+  streak: Type.Integer(),
+  prompt_weekly: Type.Boolean(),
+});
 
 type DailyCheckInSubmission = Static<typeof DailyCheckInSubmission>;
 type DailyCheckInResponse = Static<typeof DailyCheckInResponse>;
 
 const dailyCheckin: FastifyPluginAsync = async (fastify, _): Promise<void> => {
-    // @ts-ignore
-    fastify.addHook("onRequest", fastify.authenticate);
+  // @ts-ignore
+  fastify.addHook("onRequest", fastify.authenticate);
 
-    fastify.get("/fetch-screen-1", () => {
-        return CHECKINPROMTS.DAILY_CHECK_FEELING;
-    })
+  fastify.get("/fetch-screen-1", () => {
+    return CHECKINPROMTS.DAILY_CHECK_FEELING;
+  });
 
-    fastify.get("/fetch-screen-2", () => {
-        return CHECKINPROMTS.MOOD_DESCRIPTION;
-    })
+  fastify.get("/fetch-screen-2", () => {
+    return CHECKINPROMTS.MOOD_DESCRIPTION;
+  });
 
-    fastify.get("/fetch-screen-3", () => {
-        return CHECKINPROMTS.MOOD_DESCRIPTION;
-    })
+  fastify.get("/fetch-screen-3", () => {
+    return CHECKINPROMTS.MOOD_DESCRIPTION;
+  });
 
-    fastify.get("/", 
-        {
-            schema: {
-                response: {
-                    200: Type.Union([DailyCheckInSubmission, Type.Object({})])
-                }
-            }
-        }, 
-        async (request, _) => {
-            //@ts-ignore
-            const userId = request.user.sub;
-            const checkinRecords = await fastify.db.query.dailyCheckIn.findFirst({
-                where: and(
-                    eq(dailyCheckIn.userId, userId),
-                    eq(sql`DATE(${dailyCheckIn.createdAt})`, sql`DATE(NOW())`)
-                )
-            })
+  fastify.get(
+    "/",
+    {
+      schema: {
+        response: {
+          200: Type.Union([DailyCheckInSubmission, Type.Object({})]),
+        },
+      },
+    },
+    async (request, _) => {
+      //@ts-ignore
+      const userId = request.user.sub;
+      const checkinRecords = await fastify.db.query.dailyCheckIn.findFirst({
+        where: and(
+          eq(dailyCheckIn.userId, userId),
+          eq(sql`DATE(${dailyCheckIn.createdAt})`, sql`DATE(NOW())`),
+        ),
+      });
 
-            if (checkinRecords) {
-                return checkinRecords;
-            }
+      if (checkinRecords) {
+        return checkinRecords;
+      }
 
-            return {}
-        }
-    )
+      return {};
+    },
+  );
 
-    fastify.post<{ Body: DailyCheckInSubmission }>("/", 
-        {
-            schema: {
-                body: DailyCheckInSubmission,
-                response: {
-                    201: DailyCheckInResponse
-                }
-            }
-        }, 
-        async (request, reply) => {
-            const checkinResult = await fastify.db.transaction(async (trx) => {
-                try {
-                    // @ts-ignore
-                    const userId = request.user.sub;
+  fastify.post<{ Body: DailyCheckInSubmission }>(
+    "/",
+    {
+      schema: {
+        body: DailyCheckInSubmission,
+        response: {
+          201: DailyCheckInResponse,
+        },
+      },
+    },
+    async (request, reply) => {
+      const checkinResult = await fastify.db.transaction(async (trx) => {
+        try {
+          // @ts-ignore
+          const userId = request.user.sub;
 
-                    await trx.insert(dailyCheckIn)
-                    .values({
-                        // @ts-ignore
-                        id: randomUUID(),
-                        howAreYouFeeling: request.body.how_are_you_feeling,
-                        moodDescription: request.body.mood_description,
-                        moodDescriptionCauseCategory1: request.body.mood_description_cause_category_1,
-                        moodDescriptionCauseResponse1: request.body.mood_description_cause_response_1,
-                        moodDescriptionCauseCategory2: request.body.mood_description_cause_category_2,
-                        moodDescriptionCauseResponse2: request.body.mood_description_cause_response_2,
-                        moodDescriptionCauseCategory3: request.body.mood_description_cause_category_3,
-                        moodDescriptionCauseResponse3: request.body.mood_description_cause_response_3,
-                        userId
-                    });
-                    const rewardHubRecord = await trx.query.userRewardHub.findFirst({
-                        where: eq(userRewardHub.userId, userId)
-                    });
+          await trx.insert(dailyCheckIn).values({
+            // @ts-ignore
+            id: randomUUID(),
+            howAreYouFeeling: request.body.how_are_you_feeling,
+            moodDescription: request.body.mood_description,
+            moodDescriptionCauseCategory1:
+              request.body.mood_description_cause_category_1,
+            moodDescriptionCauseResponse1:
+              request.body.mood_description_cause_response_1,
+            moodDescriptionCauseCategory2:
+              request.body.mood_description_cause_category_2,
+            moodDescriptionCauseResponse2:
+              request.body.mood_description_cause_response_2,
+            moodDescriptionCauseCategory3:
+              request.body.mood_description_cause_category_3,
+            moodDescriptionCauseResponse3:
+              request.body.mood_description_cause_response_3,
+            userId,
+          });
+          const rewardHubRecord = await trx.query.userRewardHub.findFirst({
+            where: eq(userRewardHub.userId, userId),
+          });
 
-                    if (!rewardHubRecord) {
-                        // do something
-                    }
+          if (!rewardHubRecord) {
+            // do something
+          }
 
-                    const userAchievementRecord = await trx.query.userAchievement.findFirst({
-                        // @ts-ignore
-                        where: eq(userAchievement.userRewardHubId, rewardHubRecord?.id)
-                    });
+          const userAchievementRecord =
+            await trx.query.userAchievement.findFirst({
+              // @ts-ignore
+              where: eq(userAchievement.userRewardHubId, rewardHubRecord?.id),
+            });
 
-                    if (!userAchievementRecord) {
-                        // do something
-                    }
+          if (!userAchievementRecord) {
+            // do something
+          }
 
-                    // update reward hub record
-
-                } catch (error) {
-                    
-                }
-            })
-            return reply.code(201).send(checkinResult);
-        }
-    )
-}
+          // update reward hub record
+        } catch (error) {}
+      });
+      return reply.code(201).send(checkinResult);
+    },
+  );
+};
 
 export default dailyCheckin;
 
