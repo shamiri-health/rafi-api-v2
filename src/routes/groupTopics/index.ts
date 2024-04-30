2dc2828f61577d4c6a6f360d651b409d9daf6e38
UwaoEvan <uwaoevanz@gmail.com>
2024-04-30 09:36:57 +0300

    formatting changes

--- src/routes/groupTopics/index.ts
+++ src/routes/groupTopics/index.ts
@@ -4,30 +4,31 @@ import { groupTopic } from "../../database/schema";
 import { Type, Static } from "@sinclair/typebox";
 
 const GroupTopic = Type.Object({
-    id: Type.Number(),
-    name: Type.String(),
-    about: Type.String(),
-    summary: Type.String()
-})
+  id: Type.Number(),
+  name: Type.String(),
+  about: Type.String(),
+  summary: Type.String(),
+});
 
 type GroupTopic = Static<typeof GroupTopic>;
 
 const groupTopics: FastifyPluginAsync = async (fastify, _): Promise<void> => {
-    fastify.get("/", 
-        {
-            schema: {
-                response: {
-                    200: Type.Array(GroupTopic)
-                }
-            }
-        }, 
-        async () => {
-            const groupTopics = await fastify.db.query.groupTopic.findMany({
-                orderBy: asc(groupTopic.id)
-            })
-            
-            return groupTopics;
-        }
-    )
-}
+  fastify.get(
+    "/",
+    {
+      schema: {
+        response: {
+          200: Type.Array(GroupTopic),
+        },
+      },
+    },
+    async () => {
+      const groupTopics = await fastify.db.query.groupTopic.findMany({
+        orderBy: asc(groupTopic.id),
+      });
+
+      return groupTopics;
+    },
+  );
+};
 export default groupTopics;
