bea2ae9376d4a572eee4868db5d40ec5d65d5600
UwaoEvan <uwaoevanz@gmail.com>
2024-04-30 10:06:57 +0300

    migrate the daily checkin endpoints

--- src/routes/daily-check-in/index.ts
+++ src/routes/daily-check-in/index.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginAsync } from "fastify";
 import CHECKINPROMTS from "../../../static/daily_checkin_prompts.json";
-import { eq, sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { dailyCheckIn, userAchievement, userRewardHub } from "../../database/schema";
 import { Static, Type } from "@sinclair/typebox";
 import { randomUUID } from "crypto";
@@ -63,106 +63,18 @@ const dailyCheckin: FastifyPluginAsync = async (fastify, _): Promise<void> => {
         async (request, _) => {
             //@ts-ignore
             const userId = request.user.sub;
-            const query = sql`SELECT * from ${dailyCheckIn} WHERE ${dailyCheckIn.userId} = ${userId} and DATE(${dailyCheckIn.createdAt}) = DATE(NOW())`;
-            const [checkinRecords] = await fastify.db.execute(query);
+            const checkinRecords = await fastify.db.query.dailyCheckIn.findFirst({
+                where: and(
+                    eq(dailyCheckIn.userId, userId),
+                    eq(sql`DATE(${dailyCheckIn.createdAt})`, sql`DATE(NOW())`)
+                )
+            })
 
             if (checkinRecords) {
                 return checkinRecords;
             }
 
             return {}
-        }
-    )
-
-    fastify.post<{ Body: DailyCheckInSubmission }>("/", 
-        {
-            schema: {
-                body: DailyCheckInSubmission,
-                response: {
-                    201: DailyCheckInResponse
-                }
-            }
-        }, 
-        async (request, reply) => {
-            const checkinResult = await fastify.db.transaction(async (trx) => {
-                try {
-                    // @ts-ignore
-                    const userId = request.user.sub;
-
-                    await trx.insert(dailyCheckIn)
-                    .values({
-                        // @ts-ignore
-                        id: randomUUID(),
-                        howAreYouFeeling: request.body.how_are_you_feeling,
-                        moodDescription: request.body.mood_description,
-                        moodDescriptionCauseCategory1: request.body.mood_description_cause_category_1,
-                        moodDescriptionCauseResponse1: request.body.mood_description_cause_response_1,
-                        moodDescriptionCauseCategory2: request.body.mood_description_cause_category_2,
-                        moodDescriptionCauseResponse2: request.body.mood_description_cause_response_2,
-                        moodDescriptionCauseCategory3: request.body.mood_description_cause_category_3,
-                        moodDescriptionCauseResponse3: request.body.mood_description_cause_response_3,
-                        userId
-                    }).returning()
-
-                    const rewardHubRecord = await trx.query.userRewardHub.findFirst({
-                        where: eq(userRewardHub.userId, userId)
-                    })
-
-                    if (!rewardHubRecord) {
-                        const [userRewardHubRecord] = await trx.insert(userRewardHub)
-                        .values({
-                            level: 0,
-                            userId,
-                            gemsHave: 0
-                        }).returning()
-
-                        await trx.insert(userAchievement)
-                        .values({
-                            userRewardHubId: userRewardHubRecord.id,
-                            userId,
-                            streakUpdatedAt: new Date(),
-                        }).returning()
-                    }
-
-                    const userAchievementRecord = await trx.query.userAchievement.findFirst({
-                        // @ts-ignore
-                        where: eq(userAchievement.userId, rewardHubRecord?.id),
-                    }) 
-
-                    if (!userAchievementRecord) {
-                        await trx.insert(userAchievement)
-                        .values({
-                            streakUpdatedAt: new Date()
-                        })
-                    }
-
-                    // add 5 gems for checking in and move the user to next level if the need be
-                    const gemsHave = rewardHubRecord?.gemsHave;
-                    await trx.update(userRewardHub)
-                    .set({
-                        gemsHave: gemsHave ? gemsHave + 5 : 0
-                    })
-                    // link userRewardHub record to RewardHub table
-                    // update the streak and gems
-                    const streak = userAchievementRecord?.streak;
-                    const gems = userAchievementRecord?.gems;
-                    const streakUpdatedAt = userAchievementRecord?.streakUpdatedAt;
-
-                    await trx.update(userAchievement)
-                    .set({
-                        streak: streak ? streak + 1 : 0,
-                        gems: gems ? gems + 5 : 0,
-                        streakUpdatedAt: streakUpdatedAt ? streakUpdatedAt : new Date(Date.now())
-                    })
-                    .where(eq(userAchievement.userId, userId))
-                    .returning();
-
-                } catch (error) {
-                    
-                }
-            })
-
-            return reply.code(201).send(checkinResult);
         }
     )
 }
