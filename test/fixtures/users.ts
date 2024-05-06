713d4ae46708253b4f2530ed3fb073b098c0655c
UwaoEvan <uwaoevanz@gmail.com>
2024-05-06 12:36:29 +0300

    Creates user achivement record when generating a user

--- test/fixtures/users.ts
+++ test/fixtures/users.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { human, therapist, user } from "../../src/database/schema";
+import { human, therapist, user, userAchievement } from "../../src/database/schema";
 import type { database } from "../../src/lib/db";
 import { formatISO } from "date-fns";
 
@@ -51,6 +51,14 @@ export const generateUser = async (
     .insert(user)
     .values(data as typeof user.$inferSelect)
     .returning();
+  
+  await db
+  .insert(userAchievement)
+  .values({
+    level: 1,
+    userId: data.id
+  })
+
   return result;
 };
 
