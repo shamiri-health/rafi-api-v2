ae7e554058669a7ea3fefcc90d01e617ae2f4db5
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-05-06 11:12:02 +0300

    update subscription type API

--- src/routes/subscription-types/index.ts
+++ src/routes/subscription-types/index.ts
@@ -2,26 +2,20 @@ import { FastifyPluginAsync } from "fastify";
 import { subscriptionType } from "../../database/schema";
 import { Static, Type } from "@sinclair/typebox";
 import { randomUUID } from "node:crypto";
+import { eq } from "drizzle-orm";
 
 const SubscriptionTypeSchema = Type.Object({
   description: Type.String(),
+  price: Type.Number(),
   duration_days: Type.Optional(Type.Integer()),
   duration_months: Type.Optional(Type.Integer()),
 });
-
-const UpdateSchema = Type.Composite([
-  Type.Pick(SubscriptionTypeSchema, ["duration_days", "duration_months"]),
-  Type.Object({
-    description: Type.Optional(Type.String()),
-  }),
-]);
 
 const ParamsSchema = Type.Object({
   subscriptionTypeId: Type.String(),
 });
 
 type SubscriptionTypeSchema = Static<typeof SubscriptionTypeSchema>;
-type UpdateSchema = Static<typeof UpdateSchema>;
 type ParamsSchema = Static<typeof ParamsSchema>;
 
 // TODO: these endpoints need to be hidden by an admin auth
@@ -43,30 +37,47 @@ const subscriptionTypesRouter: FastifyPluginAsync = async (
       },
     },
     async (req) => {
+      if (!req.body.duration_months && !req.body.duration_days) {
+        throw fastify.httpErrors.badRequest('Please supply one of duration_days or duration_months')
+      }
+
+      const body: typeof subscriptionType.$inferInsert = {
+        id: randomUUID(),
+        durationDays: req.body.duration_days ?? undefined,
+        durationMonths: req.body.duration_months ?? undefined,
+        price: req.body.price,
+        description: req.body.description
+      }
       const [newSubscriptionType] = await fastify.db
         .insert(subscriptionType)
-        .values({
-          ...req.body,
-          id: randomUUID(),
-        })
+        .values(body)
         .returning();
 
       return newSubscriptionType;
     },
   );
 
-  fastify.patch<{ Body: UpdateSchema; Params: ParamsSchema }>(
-    "/:subscriptionTypeId",
-    {
-      schema: {
-        params: ParamsSchema,
-        body: UpdateSchema,
-      },
-    },
-    async (req) => {
-      const { subscriptionTypeId } = req.params;
-    },
-  );
+  fastify.post<{ Params: ParamsSchema }>('/:subscriptionTypeId/archive', async (req) => {
+    const { subscriptionTypeId } = req.params;
+
+    const existingSubscriptionType = await fastify.db.query.subscriptionType.findFirst({
+      where: eq(subscriptionType.id, subscriptionTypeId)
+    })
+
+    if (!existingSubscriptionType) {
+      throw fastify.httpErrors.notFound("Could not archive a subscription with the given id")
+    }
+
+    if (existingSubscriptionType.archivedAt) {
+      throw fastify.httpErrors.badRequest("Could not archive and already archived subscription")
+    }
+
+    const [archivedSubscription] = await fastify.db.update(subscriptionType).set({
+      archivedAt: new Date(),
+    }).returning()
+
+    return archivedSubscription
+  })
 };
 
 export default subscriptionTypesRouter;
