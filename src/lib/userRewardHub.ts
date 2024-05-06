6c2e261cd39cdd684fde81e7728a759bce6f2e22
UwaoEvan <uwaoevanz@gmail.com>
2024-05-06 10:37:31 +0300

    Updated daily checkin api

--- src/lib/userRewardHub.ts
+++ src/lib/userRewardHub.ts
@@ -37,63 +37,71 @@ export const createRewardHubRecord = async (
   nGems: number,
 ) => {
   const totalGems: number = record.gems + nGems;
-  const gemsNextLevel: number = gemsLevel[record.level + 1][1];
+  const level = record.level ? record.level : 1;
+  const gemsNextLevel: number = gemsLevel[level + 1][1];
+  const levelName: string = gemsLevel[level][0];
   const updatedStreak: number = getUserStreak(record);
+  try {
+    await db
+    .insert(rewardHubRecord)
+    .values({
+      level,
+      levelName,
+      streak: updatedStreak,
+      gemsHave: totalGems,
+      gemsNextLevel: gemsNextLevel,
+      userId: record.userId,
+      timestamp: new Date()
+    });
 
-  await db
+    const [postedAchievement] = await db
     .update(userAchievement)
     .set({
       gems: totalGems,
       streak: updatedStreak,
       streakUpdatedAt: new Date(),
-      level: record.level,
+      level,
     })
-    .where(eq(userAchievement.id, record.id));
-
-  await db.insert(rewardHubRecord).values({
-    level: record.level,
-    levelName: gemsLevel[record.level][0],
-    streak: updatedStreak,
-    gemsHave: record.gems,
-    gemsNextLevel: gemsNextLevel,
-    userId: record.userId,
-  });
-
-  if (totalGems >= gemsNextLevel) {
-    return await unlockNextLevel(db, record);
+    .where(eq(userAchievement.id, record.id))
+    .returning();
+    
+    if (totalGems >= gemsNextLevel) {
+      // @ts-ignore
+      return await unlockNextLevel(db, postedAchievement);
+    }
+    return postedAchievement;
+  } catch (error) {
+    throw error;
   }
-  return {};
 };
 
 const unlockNextLevel = async (db: database["db"], record: RewardHub) => {
-  if (record.level > 9) return;
-
+  if (record.level > 9) return record;
   const nextLevel: number = record.level + 1;
+ try {
+    await db.insert(rewardHubRecord).values({
+      level: nextLevel,
+      levelName: gemsLevel[nextLevel][0],
+      gemsNextLevel: gemsLevel[nextLevel + 1][1],
+    });
 
-  await db.insert(rewardHubRecord).values({
-    level: nextLevel,
-    levelName: gemsLevel[nextLevel][0],
-    gemsNextLevel: gemsLevel[nextLevel + 1][1],
-  });
+    const [achievement] = await db
+      .update(userAchievement)
+      .set({ level: nextLevel })
+      .where(eq(userAchievement.id, record.id))
+      .returning();
 
-  const [achievement] = await db
-    .update(userAchievement)
-    .set({ level: nextLevel })
-    .where(eq(userAchievement.id, record.id))
-    .returning();
-
-  return {
-    displayText: `You have just reached to level ${achievement.level}`,
-    level: achievement.level,
-    achievement: `achLevel ${achievement.level}`,
-  };
+    return achievement;
+ } catch (error) {
+    throw error;
+ }
 };
 
 const getUserStreak = (record: RewardHub) => {
   const NUMBER_OF_SECONDS = 86400; // seconds in 24 hours
-  const lastStreakUpdate = new Date(`${record.streakUpdatedAt}`).getTime();
-  const currentStreakUpdate = new Date().getTime();
-  let currentSteak = record.streak;
+  const lastStreakUpdate: number = record.streakUpdatedAt ? new Date(`${record.streakUpdatedAt}`).getTime() : 0;
+  const currentStreakUpdate: number = new Date().getTime();
+  let currentSteak: number = record.streak;
 
   if ((currentStreakUpdate - lastStreakUpdate) / 1000 < NUMBER_OF_SECONDS) {
     return (currentSteak += 1);
---
+++ test/routes/daily-check-in/create-daily-checkin-record.test.ts
@@ -0,0 +1,54 @@
+import { test } from "tap";
+import { and, eq, sql } from "drizzle-orm";
+import { faker } from "@faker-js/faker";
+import { build } from "../../helper";
+import { generateUser } from "../../fixtures/users";
+import { encodeAuthToken } from "../../../src/lib/utils/jwt";
+import { dailyCheckIn, rewardHubRecord, user, userAchievement } from "../../../src/database/schema";
+import { generateRewardHubRecord, generateUserAchievement } from "../../fixtures/rewardHub";
+
+test("POST /daily-check-in should create a new daily checkin record", async (t) => {
+    const app = await build(t);
+    const sampleUser = await generateUser(app.db);
+    const token = await encodeAuthToken(sampleUser.id, "user");
+    const sampleAchievement = await generateUserAchievement(app.db, sampleUser.id);
+    // Generate a sample reward hub record
+    await generateRewardHubRecord(app.db, sampleUser.id);
+
+    t.teardown(async () => {
+        await app.db.delete(rewardHubRecord).where(eq(rewardHubRecord.userId, sampleUser.id));
+        await app.db.delete(userAchievement).where(eq(userAchievement.id, sampleAchievement.id));
+        await app.db.delete(dailyCheckIn).where(eq(dailyCheckIn.userId, sampleUser.id));
+        await app.db.delete(user).where(eq(user.id, sampleUser.id));
+    })
+
+    const payload = {
+        how_are_you_feeling: faker.lorem.word(),
+        mood_description: faker.lorem.sentence(),
+        mood_description_cause_category_1: faker.lorem.word(),
+        mood_description_cause_response_1: faker.lorem.word(),
+        mood_description_cause_category_2: faker.lorem.word(),
+        mood_description_cause_response_2: faker.lorem.word(),
+        mood_description_cause_category_3: faker.lorem.word(),
+        mood_description_cause_response_3: faker.lorem.word()
+    }
+
+    const response = await app
+    .inject()
+    .headers({ authorization: `bearer ${token}`})
+    .post("/daily-check-in")
+    .payload(payload);
+
+    const body = await response.json();
+    console.log({body})
+
+    const userAchievementRecord = await app.db.query.dailyCheckIn.findFirst({
+        where: and( 
+            eq(dailyCheckIn.userId, sampleUser.id),
+            eq(sql`DATE(${dailyCheckIn.createdAt})`, sql`DATE(NOW())`)
+        )
+    })
+    
+    t.equal(response.statusCode, 201);
+    t.ok(userAchievementRecord);
+})
