1c63e3df55ed4ec09be9fc57dff97992355e25dd
UwaoEvan <uwaoevanz@gmail.com>
2024-05-06 15:40:13 +0300

    Updated the reward hub logic

--- src/lib/userRewardHub.ts
+++ src/lib/userRewardHub.ts
@@ -1,24 +1,14 @@
-import { Static, Type } from "@sinclair/typebox";
 import { eq } from "drizzle-orm";
 import { database } from "./db";
 import { rewardHubRecord, userAchievement } from "../database/schema";
-
-const RewardHub = Type.Object({
-  id: Type.Number(),
-  gems: Type.Number(),
-  level: Type.Number(),
-  streak: Type.Number(),
-  userId: Type.Number(),
-  streakUpdatedAt: Type.Optional(Type.String({ format: "date-time" })),
-});
-
-type RewardHub = Static<typeof RewardHub>;
 
 interface GemLevel {
   [level: number]: [level: string, gems: number];
 }
 
-const gemsLevel: GemLevel = {
+type AchievementRecord = typeof userAchievement.$inferSelect;
+
+const GEMS_LEVEL: GemLevel = {
   0: ["", 0],
   1: ["Worm", 5],
   2: ["Samaki", 30],
@@ -34,22 +24,23 @@ const gemsLevel: GemLevel = {
 
 export const createRewardHubRecord = async (
   db: database["db"],
-  record: RewardHub,
+  achivementRecord: AchievementRecord,
   nGems: number,
 ) => {
-  const totalGems: number = record.gems + nGems;
-  const level = record.level ? record.level : 1;
-  const gemsNextLevel: number = gemsLevel[level + 1][1];
-  const levelName: string = gemsLevel[level][0];
-  const updatedStreak: number = getUserStreak(record);
+  // @ts-ignore
+  const totalGems = achivementRecord.gems + nGems;
+  const currentLevel = achivementRecord.level ? achivementRecord.level : 1;
+  const gemsNextLevel = GEMS_LEVEL[currentLevel + 1][1];
+  const currentLevelName = GEMS_LEVEL[currentLevel][0];
+  const updatedStreak: number = getUserStreak(achivementRecord);
   try {
     await db.insert(rewardHubRecord).values({
-      level,
-      levelName,
+      level: currentLevel,
+      levelName: currentLevelName,
       streak: updatedStreak,
       gemsHave: totalGems,
       gemsNextLevel: gemsNextLevel,
-      userId: record.userId,
+      userId: achivementRecord.userId,
       timestamp: new Date(),
     });
 
@@ -59,10 +50,13 @@ export const createRewardHubRecord = async (
         gems: totalGems,
         streak: updatedStreak,
         streakUpdatedAt: new Date(),
-        level,
+        level: currentLevel,
       })
-      .where(eq(userAchievement.id, record.id))
+      .where(eq(userAchievement.id, achivementRecord.id))
       .returning();
+
+    //@ts-ignore
+    if (postedAchievement.level > 9) return postedAchievement;
 
     if (totalGems >= gemsNextLevel) {
       // @ts-ignore
@@ -74,20 +68,23 @@ export const createRewardHubRecord = async (
   }
 };
 
-const unlockNextLevel = async (db: database["db"], record: RewardHub) => {
-  if (record.level > 9) return record;
-  const nextLevel: number = record.level + 1;
+const unlockNextLevel = async (
+  db: database["db"],
+  achivementRecord: AchievementRecord,
+) => {
+  // @ts-ignore
+  const nextLevel = achivementRecord.level + 1;
   try {
     await db.insert(rewardHubRecord).values({
       level: nextLevel,
-      levelName: gemsLevel[nextLevel][0],
-      gemsNextLevel: gemsLevel[nextLevel + 1][1],
+      levelName: GEMS_LEVEL[nextLevel][0],
+      gemsNextLevel: GEMS_LEVEL[nextLevel + 1][1],
     });
 
     const [achievement] = await db
       .update(userAchievement)
       .set({ level: nextLevel })
-      .where(eq(userAchievement.id, record.id))
+      .where(eq(userAchievement.id, achivementRecord.id))
       .returning();
 
     return achievement;
@@ -96,16 +93,16 @@ const unlockNextLevel = async (db: database["db"], record: RewardHub) => {
   }
 };
 
-const getUserStreak = (record: RewardHub) => {
+const getUserStreak = (achivementRecord: AchievementRecord) => {
   const NUMBER_OF_SECONDS = 86400; // seconds in 24 hours
-  const lastStreakUpdate: number = record.streakUpdatedAt
-    ? new Date(`${record.streakUpdatedAt}`).getTime()
+  const lastStreakUpdate: number = achivementRecord.streakUpdatedAt
+    ? new Date(`${achivementRecord.streakUpdatedAt}`).getTime()
     : 0;
   const currentStreakUpdate: number = new Date().getTime();
-  let currentSteak: number = record.streak;
 
   if ((currentStreakUpdate - lastStreakUpdate) / 1000 < NUMBER_OF_SECONDS) {
-    return (currentSteak += 1);
+    // @ts-ignore
+    return achivementRecord.streak + 1;
   }
 
   return 1;
