7b202a8e11628b613934a2f1f7e6f7992dcf1ffd
UwaoEvan <74808111+UwaoEvan@users.noreply.github.com>
2024-05-06 15:48:03 +0300

    Delete src/database/0007_bumpy_tombstone.sql

--- src/database/0007_bumpy_tombstone.sql
+++
@@ -1,5 +0,0 @@
--- Custom SQL migration file, put you code below! --
-UPDATE "userAchievement"
-SET "user_id" = "userRewardHub"."userId"
-FROM "userRewardHub"
-WHERE "userAchievement"."userRewardHubId" = "userRewardHub"."id"
