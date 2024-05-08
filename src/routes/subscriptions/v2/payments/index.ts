0714c8ddaf69d00e60d569ab82c53fc0e4771c6a
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-05-08 11:52:38 +0300

    wip

--- src/routes/subscriptions/v2/payments/index.ts
+++ src/routes/subscriptions/v2/payments/index.ts
@@ -1,4 +1,5 @@
 import { Static, Type } from "@sinclair/typebox";
+import { format } from "date-fns";
 import { FastifyPluginAsync } from "fastify";
 
 const MpesaCallbackResponse = Type.Object({
@@ -9,6 +10,8 @@ const MpesaCallbackResponse = Type.Object({
       ResultCode: Type.Number(),
       ResultDesc: Type.String(),
       CallbackMetadata: Type.Optional(
+        Type.Any(),
+        /*
         Type.Object({
           Item: Type.Array(
             Type.Object({
@@ -17,6 +20,7 @@ const MpesaCallbackResponse = Type.Object({
             }),
           ),
         }),
+        */
       ),
     }),
   }),
@@ -25,7 +29,99 @@ const MpesaCallbackResponse = Type.Object({
 type MpesaCallbackResponse = Static<typeof MpesaCallbackResponse>;
 
 const paymentsRouter: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.post("/purchase-subscription", async (req) => {});
+  // TODO: return to POST
+  fastify.get("/purchase-subscription", async (req) => {
+    // base64 string of CONSUMER_KEY + : + CONSUMER_SECRET
+    const tokenAuth = btoa(
+      `${process.env.MPESA_CONSUMER_KEY}:${process.env.MPESA_CONSUMER_SECRET}`,
+    );
+
+    const accessTokenResponse = await fetch(
+      "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      {
+        headers: {
+          Authorization: `Basic ${tokenAuth}`,
+        },
+        method: "GET",
+      },
+    );
+
+    if (!accessTokenResponse.ok) {
+      fastify.log.error("Could not fetch safaricom authentication token");
+      throw fastify.httpErrors.internalServerError(
+        "Sorry, the MPESA request could not be completed at this time",
+      );
+    }
+
+    const accessTokenBody: { access_token: string; expires_in: string } =
+      await accessTokenResponse.json();
+
+    const timestamp = format(new Date(), "yyyyMMddHHmmss");
+
+    // A base64 encoded string. (The base64 string is a combination of Shortcode+Passkey+Timestamp)
+    const password = btoa(
+      `${process.env.MPESA_SHORTCODE}${process.env.DARAJA_PASSKEY}${timestamp}`,
+    );
+
+    const body = JSON.stringify({
+      BusinessShortCode: process.env.MPESA_SHORTCODE,
+      Password: password,
+      Timestamp: timestamp,
+      TransactionType: "CustomerPayBillOnline",
+      Amount: "1",
+      PartyA: "254717266218",
+      PartyB: process.env.MPESA_SHORTCODE,
+      PhoneNumber: "254717266218",
+      CallBackURL:
+        "https://cnet-comp-certificate-montreal.trycloudflare.com/subscriptions/v2/payments/mpesa-callback",
+      AccountReference: "Test",
+      TransactionDesc: "Test",
+    });
+
+    const res = await fetch(
+      "https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessTokenBody.access_token}`,
+        },
+        method: "POST",
+        body,
+      },
+    );
+
+    if (!res.ok) {
+      fastify.log.error(res.statusText);
+      throw fastify.httpErrors.internalServerError(
+        "Could not process MPESA payment request at this time",
+      );
+    }
+
+    type MpesaResponse = {
+      MerchantRequestID: string;
+      CheckoutRequestID: string;
+      ResponseCode: string;
+      ResponseDescription: string;
+      CustomerMessage: string;
+    };
+
+    const mpesaBody: MpesaResponse = await res.json();
+
+    if (mpesaBody.ResponseCode !== "0") {
+      // the transaction cannot be initiated or there was an error
+      // see: https://developer.safaricom.co.ke/APIs/MpesaExpressSimulate
+      fastify.log.error(mpesaBody.ResponseDescription);
+      throw fastify.httpErrors.internalServerError(
+        "Could not process MPESA payment request at this time",
+      );
+    }
+
+    // TODO: need to save this MPESA ref in the payments table
+
+    return {
+      message: "MPESA transaction successfully initiated",
+    };
+  });
 
   // this should only whitelist the APIs from safaricom
   fastify.post<{ Body: MpesaCallbackResponse }>(
