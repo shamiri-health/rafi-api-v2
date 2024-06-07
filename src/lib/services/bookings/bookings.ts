11e1e24efcca03c99f94e01d5eefb41826d9cd80
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-06-07 15:11:00 +0300

    add google libraries

--- src/lib/services/bookings/bookings.ts
+++ src/lib/services/bookings/bookings.ts
@@ -1,5 +1,19 @@
+import { eq } from "drizzle-orm";
 import type { database } from "../../db";
+import { userService } from "../../../database/schema";
+import { sample } from "lodash";
 
-export async function createOnsiteOrder(db: database["db"]) {}
+export async function createOnsiteOrder(db: database["db"], userId: number, startTime: Date, endTime: Date) {
+  const userServiceRecord = await db.query.userService.findFirst({
+    where: eq(userService.userId, userId)
+  })
 
-export async function createTeletherapyOrder(db: database["db"]) {}
+  let therapistId = userServiceRecord?.assignedTherapistId;
+
+  if (!therapistId) {
+    therapistId = sample([10, 205])
+  }
+
+}
+
+export async function createTeletherapyOrder(db: database["db"], userId: number, startTime: Date, endTime: Date) { }
