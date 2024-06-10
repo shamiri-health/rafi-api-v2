eed7b3610225b96ef941e3c79cae164170a12f6f
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-06-10 13:05:53 +0300

    add more function stubs

--- src/routes/booking/index.ts
+++ src/routes/booking/index.ts
@@ -6,61 +6,93 @@ import {
   createTeletherapyEvent,
 } from "../../lib/services/bookings/bookings";
 import { formatISO } from "date-fns";
+import { Static, Type } from "@sinclair/typebox";
+
+const EventBody = Type.Object({
+  start_time: Type.String({ format: "date-time" }),
+  end_time: Type.String({ format: "end-time" }),
+  therapist_id: Type.Optional(Type.String()),
+  event_id: Type.Optional(Type.String()),
+});
+
+type EventBody = Static<typeof EventBody>;
 
 const bookingRouter: FastifyPluginAsync = async (fastify): Promise<void> => {
   // @ts-ignore
   fastify.addHook("onRequest", fastify.authenticate);
 
-  fastify.post("/onsite", async (req) => {
-    // @ts-ignore
-    const userId: number = req.user.sub;
+  fastify.post<{ Body: EventBody }>(
+    "/onsite",
+    { schema: { body: EventBody } },
+    async (req) => {
+      // @ts-ignore
+      const userId: number = req.user.sub;
 
-    const currentUser = await fastify.db.query.user.findFirst({
-      where: eq(user.id, userId),
-    });
-
-    if (!currentUser) {
-      throw fastify.httpErrors.notFound(`User with id: ${userId} not found`);
-    }
-
-    const existingGroupPlan = await fastify.db.query.groupPlan.findFirst({
-      where: eq(groupPlan.clientId, user.clientId),
-    });
-
-    if (existingGroupPlan && existingGroupPlan.expireTime > new Date()) {
-      const response = await createOnsiteEvent(fastify.db);
-
-      return response;
-    }
-
-    const existingSubscription =
-      await fastify.db.query.subscriptionV2.findFirst({
-        where: and(
-          eq(subscriptionV2.userId, userId),
-          isNull(subscriptionV2.cancelledAt),
-          gte(subscriptionV2.endDate, new Date().toISOString()),
-        ),
+      const currentUser = await fastify.db.query.user.findFirst({
+        where: eq(user.id, userId),
       });
 
-    if (!existingSubscription) {
-      throw fastify.httpErrors.badRequest(
-        "Cannot book an onsite session if the user does not have a valid subscription",
-      );
-    }
+      if (!currentUser) {
+        throw fastify.httpErrors.notFound(`User with id: ${userId} not found`);
+      }
 
-    const event = await createOnsiteEvent(fastify.db);
+      const existingGroupPlan = await fastify.db.query.groupPlan.findFirst({
+        where: eq(groupPlan.clientId, user.clientId),
+      });
 
-    if (existingSubscription.isOneOff) {
-      await fastify.db
-        .update(subscriptionV2)
-        .set({
-          endDate: formatISO(new Date(), { representation: "date" }),
-        })
-        .where(eq(subscriptionV2.id, existingSubscription.id));
-    }
+      if (existingGroupPlan && existingGroupPlan.expireTime > new Date()) {
+        if (req.body.event_id) {
+          // update onsite event
+          // return event
+        }
+        return await createOnsiteEvent(
+          fastify.db,
+          req.user.sub,
+          new Date(req.body.start_time),
+          new Date(req.body.end_time),
+        );
+      }
 
-    return event;
-  });
+      const existingSubscription =
+        await fastify.db.query.subscriptionV2.findFirst({
+          where: and(
+            eq(subscriptionV2.userId, userId),
+            isNull(subscriptionV2.cancelledAt),
+            gte(subscriptionV2.endDate, new Date().toISOString()),
+          ),
+        });
+
+      if (!existingSubscription) {
+        throw fastify.httpErrors.badRequest(
+          "Cannot book an onsite session if the user does not have a valid subscription",
+        );
+      }
+
+      let event;
+      if (req.body.event_id) {
+        // update onsite event
+        // update event
+      } else {
+        event = await createOnsiteEvent(
+          fastify.db,
+          req.user.sub,
+          new Date(req.body.start_time),
+          new Date(req.body.end_time),
+        );
+      }
+
+      if (existingSubscription.isOneOff) {
+        await fastify.db
+          .update(subscriptionV2)
+          .set({
+            endDate: formatISO(new Date(), { representation: "date" }),
+          })
+          .where(eq(subscriptionV2.id, existingSubscription.id));
+      }
+
+      return event;
+    },
+  );
 
   fastify.post("/teletherapy", async (req) => {
     // @ts-ignore
